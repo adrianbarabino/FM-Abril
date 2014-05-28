c388d619a54f22bf74858babbaab816186480cb9
Adrian Barabino <adrian.barabino@gmail.com>
2014-05-28 12:16:35 -0300

    "Upload" method added to media class
    Copied an example of upload class from a website and added it to the
    project

--- classes/Media.class.php
+++ classes/Media.class.php
@@ -11,6 +11,113 @@ require_once './classes/Misc.class.php';
 class Media extends Misc {
 
 
+	// BEGIN of Code from: http://www.devshed.com/c/a/php/developing-a-modular-class-for-a-php-file-uploader/ 
+	// By AlejandroGervasio on April 16, 2008
+
+	private $uploadFile;
+	private $name;
+	private $tmp_name;
+	private $type;
+	private $size;
+	private $error;
+
+	private $allowedTypes=array
+	('image/jpeg','image/gif','audio/ogg','audio/mpeg','audio/mp3','audio/wav','image/png','text/plain','application/ms-word');
+
+	public function __construct($uploadDir='./uploads/'){
+
+		if(!is_dir($uploadDir)){
+
+			throw new Exception('Invalid upload directory.');
+
+		}
+
+		if(!count($_FILES)){
+
+			throw new Exception('Invalid number of file upload parameters.');
+
+		}
+
+		foreach($_FILES['userfile'] as $key=>$value){
+
+			$this->{$key}=$value;
+
+		}
+
+		if(!in_array($this->type,$this->allowedTypes)){
+			print_r("El tipo es: ".$this->type);
+			throw new Exception('Invalid MIME type of target file.');
+
+		}
+		$folder = explode('/', $this->type);
+		print_r($folder);
+		$this->uploadFile=$uploadDir.$folder[0].'/'.basename($this->name);
+
+	}
+
+	// upload target file to specified location
+
+	public function upload(){
+
+		if(move_uploaded_file($this->tmp_name,$this->uploadFile)){
+
+			return true;
+
+		}
+
+		// throw exception according to error number
+
+		switch($this->error){
+
+			case 1:
+
+			throw new Exception('Target file exceeds maximum allowed size.');
+
+			break;
+
+			case 2:
+
+			throw new Exception('Target file exceeds the MAX_FILE_SIZE value specified on the upload form.');
+
+			break;
+
+			case 3:
+
+			throw new Exception('Target file was not uploaded completely.');
+
+			break;
+
+			case 4:
+
+			throw new Exception('No target file was uploaded.');
+
+			break;
+
+			case 6:
+
+			throw new Exception('Missing a temporary folder.');
+
+			break;
+
+			case 7:
+
+			throw new Exception('Failed to write target file to disk.');
+
+			break;
+
+			case 8:
+
+			throw new Exception('File upload stopped by extension.');
+
+			break;
+
+		}
+
+	}
+
+	// END of Code from: http://www.devshed.com/c/a/php/developing-a-modular-class-for-a-php-file-uploader/ 
+
+
     public function isExist($id_media)
     {
     	// print_r($this->_db);
@@ -75,3 +182,8 @@ class Media extends Misc {
 		}
     }
 }
+
+
+
+
+
